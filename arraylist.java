b41a3fdaf10bb09a6db24c25f9d4a5b7127d505d
Hitanshi-Singh <tech.hitanshi@gmail.com>
2023-05-26 20:15:00 +0530

    arraylist

---
+++ arraylist.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class arraylist {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>(10);
+        for (int i = 0; i < 3; i++) {
+            list.add(89);
+        }
+        System.out.println(list);
+
+    }
+}
