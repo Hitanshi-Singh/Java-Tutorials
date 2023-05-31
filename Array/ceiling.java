bc201f375ee3a145bfa66e8b49df0dd3dc81f248
Hitanshi-Singh <tech.hitanshi@gmail.com>
2023-05-31 23:09:17 +0530

    resolves the problems

--- Array/ceiling.java
+++ Array/ceiling.java
@@ -1,4 +1,3 @@
-import java.util.*;
 
 public class ceiling {
     public static void main(String[] args) {
--- Array/ceiling_letter.java
+++ Array/ceiling_letter.java
@@ -1,8 +1,6 @@
-import java.util.*;
 
 public class ceiling_letter {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         char arr[] = { 'a', 'c', 'e', 'g', 'h', 'l', 'p' };
         char target = 'd';
         System.out.println(ceil(arr, target));
