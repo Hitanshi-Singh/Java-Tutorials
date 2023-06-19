53a4b49373799c56caebb8c018aa44f01e459b01
Hitanshi-Singh <tech.hitanshi@gmail.com>
2023-06-19 18:15:25 +0530

    leetcode 74

--- twoDArrays/sorted.java
+++ twoDArrays/sorted.java
@@ -1,5 +1,25 @@
 package twoDArrays;
+//https://leetcode.com/problems/search-a-2d-matrix/description/
 
-public class sorted {
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int r = 0, c = matrix[0].length - 1;
 
+        while (c >= 0 && r <= matrix.length - 1) {
+            if (target == matrix[r][c]) {
+
+                return true;
+
+            }
+            if (target < matrix[r][c]) {
+                c--;
+            } else {
+                r++;
+
+            }
+        }
+
+        return false;
+
+    }
 }
