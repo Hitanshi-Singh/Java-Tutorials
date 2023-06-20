43c3e5e79dd4de0b592af1b1241ca87c525cdcce
Hitanshi-Singh <tech.hitanshi@gmail.com>
2023-06-20 22:32:13 +0530

    sorted matrix

---
+++ twoDArrays/sortedMatrix.java
@@ -0,0 +1,23 @@
+package twoDArrays;
+
+public class sortedMatrix {
+    public static void main(String[] args) {
+
+    }
+
+    // search in the row provided
+    static int[] binarySearch(int[][] matrix, int row, int cStart, int cEnd, int target) {
+        while (cStart <= cEnd) {
+            int mid = cStart + (cEnd - cStart) / 2;
+            if (matrix[row][mid] == target) {
+                return new int[] { row, mid };
+            }
+            if (matrix[row][mid] < target) {
+                cStart = mid + 1;
+            } else {
+                cEnd = mid - 1;
+            }
+        }
+        return new int[] { -1, -1 };
+    }
+}
