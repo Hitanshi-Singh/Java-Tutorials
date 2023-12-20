e53a8ae77c742118c701a3b1fa5067390fee1dcf
Hitanshi-Singh <tech.hitanshi@gmail.com>
2023-12-20 10:34:34 +0530

    backtracking eg

---
+++ Alpha Plus/26_BackTracking/Array.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+// package 26_BackTracking;
+
+public class Array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n : ");
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        backtrack(n - 1, a, n - 1);
+        System.out.println(Arrays.toString(a));
+
+    }
+
+    public static void backtrack(int n, int a[], int i) {
+        if (i == -1) {
+            System.out.println(Arrays.toString(a));
+            return;
+        }
+        a[i] = i + 1;
+        backtrack(n, a, i - 1);
+        a[i] -= 2;
+
+    }
+
+}
